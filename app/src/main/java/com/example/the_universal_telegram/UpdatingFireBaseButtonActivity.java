6363f9c7a20c61882d47fa8c574eabcb90d5bf51
brandon11782 <bneese@live.com>
2019-04-20 20:33:40 -0400

    Added firebase DB connection

---
+++ app/src/main/java/com/example/the_universal_telegram/UpdatingFireBaseButtonActivity.java
@@ -0,0 +1,10 @@
+package com.example.the_universal_telegram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UpdatingFireBaseButtonActivity extends AppCompatActivity {
+
+}
