fdc74b333929a1c1980566886f0ed19d1388f7ae
brandon11782 <bneese@live.com>
2019-04-21 16:34:48 -0400

    Progress toward a working upvote/downvote button for concept. Had to remove android:margins code in homepage layout file, was causing compile errors.

--- app/src/main/java/com/example/the_universal_telegram/UpdatingFireBaseButtonActivity.java
+++ app/src/main/java/com/example/the_universal_telegram/UpdatingFireBaseButtonActivity.java
@@ -1,24 +1,35 @@
 package com.example.the_universal_telegram;
 
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UpdatingFireBaseButtonActivity extends AppCompatActivity {
 
-    Integer count;
-    TextView counter;
-    Button incrementer;
-    Button resetter;
+    private TextView counter;
+    private Button incrementer;
+    private Button resetter;
 
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mCountDatabaseReference;
 
+    private static final String TAG = "FirebaseActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,29 +40,78 @@ public class UpdatingFireBaseButtonActivity extends AppCompatActivity {
         incrementer = (Button) findViewById(R.id.incrementer);
         resetter = (Button) findViewById(R.id.resetter);
 
-        count = 0; // TODO: This has to change from =0 to being read from the database on creation. Setting to 0 defeats the purpose of a DB.
-        // But the databse works! :)
-
-        counter.setText(count.toString());
-
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mCountDatabaseReference = mFirebaseDatabase.getReference().child("count");
 
+
         incrementer.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                count = count + 1;
-                mCountDatabaseReference.setValue(count);
-                counter.setText(count.toString());
+
             }
         });
 
         resetter.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                count = 0;
-                mCountDatabaseReference.setValue(count);
-                counter.setText(count.toString());
+
             }
         });
+    }
+
+    private void onUpVoted(DatabaseReference postRef){
+        postRef.runTransaction(new Transaction.Handler(){
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData){
+                Post p = mutableData.getValue(Post.class);
+                if (p == null) {
+                    return Transaction.success(mutableData);
+                }
+                else{
+                    p.upVoteCount = p.upVoteCount +1;
+                    p.upVotes.put(getUid(),true);
+                }
+
+                mutableData.setValue(p);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot){
+                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+            }
+        });
+    }
+
+    public class Post{
+        private int upVoteCount;
+        public String uid;
+        public Map<String,Boolean> upVotes = new HashMap<>();
+
+
+        public Post(String uid){
+            this.uid = uid;
+        }
+
+        public void setUpVotes(int num){
+            upVoteCount = num;
+        }
+
+        public int getUpVotes(){
+            return upVoteCount;
+        }
+
+        @Exclude
+        public Map<String,Object> toMap(){
+            HashMap<String,Object> result = new HashMap<>();
+            result.put("uid", uid);
+            result.put("upVotes", upVotes);
+            result.put("upVoteCount", upVoteCount);
+
+            return result;
+        }
+    }
+
+    public String getUid(){
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
 }
