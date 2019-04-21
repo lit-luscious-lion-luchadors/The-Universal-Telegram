8992a34bd5c867808567afeb5542d35a309f2ea0
brandon11782 <bneese@live.com>
2019-04-20 20:44:17 -0400

    Added intent from mainActivity to Firebase test area

--- app/src/main/java/com/example/the_universal_telegram/MainActivity.java
+++ app/src/main/java/com/example/the_universal_telegram/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button articlePageButton;
+    Button fireBaseTesterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
 
         articlePageButton  = (Button) findViewById(R.id.articlePageButton);
+        fireBaseTesterButton = (Button) findViewById(R.id.fireBaseTestButton);
 
         articlePageButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+
+        fireBaseTesterButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, UpdatingFireBaseButtonActivity.class));
             }
         });
     }
