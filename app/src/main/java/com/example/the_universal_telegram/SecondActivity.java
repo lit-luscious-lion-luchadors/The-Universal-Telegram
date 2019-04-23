ac6a2e2f787e5cb822829e4c2a5bf0472ca30733
koreyfm <mansellkf@g.cofc.edu>
2019-04-23 13:05:09 -0400

    Combined PullingNewsFromJSON with ArticlePage and made title on article page match HomePage title

--- app/src/main/java/com/example/the_universal_telegram/SecondActivity.java
+++ app/src/main/java/com/example/the_universal_telegram/SecondActivity.java
@@ -11,11 +11,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SecondActivity extends AppCompatActivity{
 
     Button homeButton;
     TextView articleBody;
     TextView articleTitle;
+    Button clearButton;
+
+    private TextView mArticles;
+    private Button requestArticles;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +40,75 @@ public class SecondActivity extends AppCompatActivity{
         setContentView(R.layout.activity_second);
 
         homeButton  = (Button) findViewById(R.id.home);
-        articleBody = (TextView) findViewById(R.id.bodyText);
+        requestArticles  = (Button) findViewById(R.id.GetArticles);
+
+        //articleBody = (TextView) findViewById(R.id.bodyText);
+
+        mArticles = (TextView) findViewById(R.id.bodyText);
+
         articleTitle = (TextView) findViewById(R.id.title);
+
+        clearButton = (Button) findViewById(R.id.clearArticles);
+
+
+        mQueue = Volley.newRequestQueue (this);
+
+        requestArticles.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                jsonPars();
+            }
+        });
 
         homeButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+
+                if (mQueue != null) {
+                    mQueue.cancelAll(this);
+                }
                 startActivity(new Intent(SecondActivity.this, MainActivity.class));
+                //mArticles.setText("");
             }
         });
+
+        clearButton.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                mArticles.setText("");
+            }
+        });
+    }
+
+    private void jsonPars() {
+        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=d8836231103a4e108eef3650ae1e8478";
+        JsonObjectRequest request = new JsonObjectRequest (Request.Method.GET, url, null,
+                new Response.Listener <JSONObject> ( ) {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray ("articles");
+
+                            for(int i = 0; i < jsonArray.length (); i++){
+                                JSONObject Articles = jsonArray.getJSONObject (i);
+
+                                String title = Articles.getString ("title");
+                                String description = Articles.getString ("description");
+                                String urlToImage = Articles.getString ("urlToImage");
+                                String content = Articles.getString("content");
+
+                                mArticles.append(title + "\n\n" + description + "\n\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace ( );
+                        }
+                    }
+                }, new Response.ErrorListener ( ) {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace ();
+            }
+        });
+        mQueue.add(request);
     }
 
     @Override
@@ -38,8 +116,9 @@ public class SecondActivity extends AppCompatActivity{
 
         super.onSaveInstanceState(savedInstanceState);
 
-        savedInstanceState.putString("articleBody",(String)articleBody.getText().toString());
-        savedInstanceState.putString("articleTitle",(String)articleTitle.getText().toString());
+        //savedInstanceState.putString("articleBody",(String)mArticles.getText().toString());
+        //savedInstanceState.putString("articleTitle",(String)articleTitle.getText().toString());
+        savedInstanceState.putString("mArticles",(String)mArticles.getText().toString());
 
     }
 
@@ -47,8 +126,9 @@ public class SecondActivity extends AppCompatActivity{
     public void onRestoreInstanceState(Bundle savedInstanceState) {
 
         super.onRestoreInstanceState(savedInstanceState);
-        articleBody.setText(savedInstanceState.getString("articleBody"));
-        articleTitle.setText(savedInstanceState.getString("articleTitle"));
+        //articleBody.setText(savedInstanceState.getString("mArticles"));
+        //articleTitle.setText(savedInstanceState.getString("articleTitle"));
+        mArticles.setText(savedInstanceState.getString("mArticles"));
 
     }
 
